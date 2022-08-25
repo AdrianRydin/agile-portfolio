a86c9bacfc73df17fafc76f6122982a8fa7f096e
Emil <emil.hagelin@medieinstitutet.se>
2022-08-25 09:37:02 +0200

    installed chakra ui and ChakraProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
+import * as React from 'react'
+
+import { ChakraProvider } from '@chakra-ui/react'
+
 function App() {
   return (
+    <ChakraProvider>
     <div className="bg-blue-300">
+   
     </div>
+    </ChakraProvider>
+
+    
   );
 }
 
