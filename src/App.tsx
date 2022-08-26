4b47a1443f1139ddbd684f698f98cc5edd177515
Edwin Körner <edwin.korner@medieinstitutet.se>
2022-08-26 13:48:06 +0200

    Removed teamcard.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -7,15 +7,11 @@ import {
   FormErrorMessage,
   FormHelperText,
 } from "@chakra-ui/react";
-import TeamCard from "./components/TeamCard";
 import ContactForm from "./components/ContactForm";
 
 function App() {
   return (
     <ChakraProvider>
-      <div className="bg-main-blue">
-        <TeamCard firstName="Felix" lastName="Bakkum" role="Lead Developer" />
-      </div>
       <div>
         <ContactForm />
       </div>
