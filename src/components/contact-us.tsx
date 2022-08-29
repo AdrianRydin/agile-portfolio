44637268d23d7df0788d5860182d5ef025199183
Edwin Körner <edwin.korner@medieinstitutet.se>
2022-08-29 10:47:00 +0200

    Adjustments to contact, findus and landingpage

--- src/components/contact-us.tsx
+++ src/components/contact-us.tsx
@@ -5,7 +5,7 @@ import ContactForm from "./ContactForm";
 function Contact() {
   return (
     <>
-    <div className="bg-main-blue flex flex-col justify-start items-center h-auto min-w-full">
+    <div className="bg-main-blue flex flex-col justify-start items-center h-auto min-w-full lg:p-8 lg:items-start">
       <p className="title text-white">CONTACT US</p>
       <p className="color-main-green small-text mb-10">
         Fill in the form and we'll reach out to you
