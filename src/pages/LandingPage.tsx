b08f05ce715dd6b6a58e391b4c58fbe3ec76f7d9
AdrianRydin <adrian.rydin@gmail.com>
2022-08-31 17:09:43 +0200

    Added some functions
    I still need to add functions, it doesn't work yet

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,8 +1,18 @@
 import { Button } from "@chakra-ui/react";
+import ContactForm from "../components/ContactForm";
 import "../index.css";
 import "../util/css/reusable.css";
+import {useRef} from 'react'
+
 
 function Landing() {
+    const ScrollDemo = () => {
+    const myRef = useRef<null | HTMLDivElement>(null)
+  
+    const handleClick = () => {
+      myRef.current?.scrollIntoView({behavior: 'smooth', block: 'end'})
+    }
+  }
   return (
     <div className="wrapper">
     <div className="bg-main-blue flex flex-col lg:flex-row pt-0 pb-6 lg:pt-32 lg:pb-20 lg:h-screen">
@@ -12,7 +22,7 @@ function Landing() {
       <p className="title text-white mt-10 text-center lg:text-left">
         WE CREATE NICE, CONVENIENT, AND PROFTITABLE WEBSITES WITH REACT
       </p>
-      <Button transition={"800ms"} _hover={{background: "#6FCF97", boxShadow: "0px 0px 30px #6FCF97"}} px='100' boxShadow={"0px 0px 30px #4E2ECF"} rounded="3xl" className="my-6 lg:mb-0 w-36 text-white" bg={'#4E2ECF'}>CONTACT US</Button>
+      <Button transition={"800ms"} _hover={{background: "#6FCF97", boxShadow: "0px 0px 30px #6FCF97"}} px='100' boxShadow={"0px 0px 30px #4E2ECF"} rounded="3xl" className="my-6 lg:mb-0 w-36 text-white" bg={'#4E2ECF'} onClick={ScrollDemo}>CONTACT US</Button>
       </div>
       <div className="h-1/2 self-center">
       <img className="landingImage" src="./img/Logo.svg" alt="" />
