9072e4896a60f2d77b891b8af90650dc986df840
AdrianRydin <adrian.rydin@gmail.com>
2022-09-01 08:55:26 +0200

    Small fixes, still not functional

--- src/pages/ContactUs.tsx
+++ src/pages/ContactUs.tsx
@@ -1,19 +1,30 @@
+import React, { useRef } from "react";
 import ContactForm from "../components/ContactForm";
 import "../index.css";
 import "../util/css/reusable.css";
 
-function Contact() {
-  return (
-    <>
-    <div className="bg-main-blue flex flex-col justify-start items-center h-auto w-full wrapper lg:items-start">
-      <p className="title text-white text-center lg:text-left">CONTACT US</p>
-      <p className="color-main-green small-text mb-10 text-center lg:text-left">
-        Fill in the form and we'll reach out to you
-      </p>
-    </div>
-    <ContactForm/>
-    </>
-  );
-}
+const bottomPageRef = useRef<HTMLDivElement>(null)
+
+const Contact = React.forwardRef((props, ref) => (
+  
+    (
+     <>
+     <div className="bg-main-blue flex flex-col justify-start items-center h-auto w-full wrapper lg:items-start" ref="bottomPage">
+       <p className="title text-white text-center lg:text-left">CONTACT US</p>
+       <p className="color-main-green small-text mb-10 text-center lg:text-left">
+         Fill in the form and we'll reach out to you
+       </p>
+     </div>
+     <ContactForm/>
+     </>
+   )
+  
+
+
+
+))
+
+
+
 
 export default Contact;
--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -2,17 +2,16 @@ import { Button } from "@chakra-ui/react";
 import ContactForm from "../components/ContactForm";
 import "../index.css";
 import "../util/css/reusable.css";
-import {useRef} from 'react'
+import {useRef, useEffect, useState} from 'react'
+import Contact from "./ContactUs";
+import React from "react";
 
 
 function Landing() {
-    const ScrollDemo = () => {
-    const myRef = useRef<null | HTMLDivElement>(null)
-  
-    const handleClick = () => {
-      myRef.current?.scrollIntoView({behavior: 'smooth', block: 'end'})
-    }
-  }
+
+ 
+const ref = React.createRef();
+
   return (
     <div className="wrapper">
     <div className="bg-main-blue flex flex-col lg:flex-row pt-0 pb-6 lg:pt-32 lg:pb-20 lg:h-screen">
@@ -22,7 +21,7 @@ function Landing() {
       <p className="title text-white mt-10 text-center lg:text-left">
         WE CREATE NICE, CONVENIENT, AND PROFTITABLE WEBSITES WITH REACT
       </p>
-      <Button transition={"800ms"} _hover={{background: "#6FCF97", boxShadow: "0px 0px 30px #6FCF97"}} px='100' boxShadow={"0px 0px 30px #4E2ECF"} rounded="3xl" className="my-6 lg:mb-0 w-36 text-white" bg={'#4E2ECF'} onClick={ScrollDemo}>CONTACT US</Button>
+      <Button transition={"800ms"} _hover={{background: "#6FCF97", boxShadow: "0px 0px 30px #6FCF97"}} px='100' boxShadow={"0px 0px 30px #4E2ECF"} rounded="3xl" className="my-6 lg:mb-0 w-36 text-white" bg={'#4E2ECF'} onClick={bottomPage}>CONTACT US</Button>
       </div>
       <div className="h-1/2 self-center">
       <img className="landingImage" src="./img/Logo.svg" alt="" />
